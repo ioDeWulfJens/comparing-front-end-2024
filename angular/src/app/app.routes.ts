dedc301dd267bdbef81914742a865b103525bc84
Jens De Wulf <jens.dewulf@iodigital.com>
2024-03-11 11:32:59 +0100

    Make page titles consistent

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from './routes/tasks/tasks.component';
 import { TasksService } from './common/tasks.service';
+import { TasksComponent } from './routes/tasks/tasks.component';
 
 export const routes: Routes = [
-    {
-        path: "",
-        title: "A: TaskHandler",
-        component: TasksComponent,
-        providers: [
-            TasksService
-        ]
-    }
+  {
+    path: '',
+    title: 'Tasky Angular',
+    component: TasksComponent,
+    providers: [TasksService],
+  },
 ];
